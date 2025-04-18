5628061103f710fc676e083286a5d51227c09286
TAMAYITO8 <binladeret@gmail.com>
2025-04-17 21:35:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def create_setup_intent():
     return jsonify(client_secret=intent.client_secret)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
